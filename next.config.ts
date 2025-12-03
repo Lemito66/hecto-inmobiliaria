54eaf001b469eb9e4a6af641cc9f45d208a73420
Eimi <lemito66@gmail.com>
2025-12-03 12:54:13 -0500

    Update .gitignore and next.config.ts for build output and image optimization settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // importante para poder usar next export
+  images: {
+    unoptimized: true, // porque GitHub Pages no soporta Optimizador de imágenes de Next
+  },
+  // Sólo si el proyecto NO está en user.github.io sino en un repo tipo user.github.io/mi-app
+  basePath: '/hecto-inmobiliaria',
+  assetPrefix: '/hecto-inmobiliaria/',
+  distDir: 'dist',
 };
 
 export default nextConfig;
